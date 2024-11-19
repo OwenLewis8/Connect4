949a3b4ead4528f409554968074c2c74f3475a56
OwenLewis8 <144383499+OwenLewis8@users.noreply.github.com>
2024-11-18 17:46:11 -0700

    Add files via upload

---
+++ player.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.assignment1;
+
+/**
+ *
+ * @author owenlewis
+ */
+public class player {
+    //create 3 players
+        player p1 = new player();
+        player p2 = new player();
+        player p3 = new player();
+        
+        
+}
